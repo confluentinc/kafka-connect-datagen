984f1596c803330a57c98a6f0cb5ca35b025c18d
Greg Harris <gregh@confluent.io>
2019-11-20 12:56:55 -0800

    Add test for custom partitioning
    Signed-off-by: Greg Harris <gregh@confluent.io>

--- src/test/java/io/confluent/kafka/connect/datagen/DatagenTaskTest.java
+++ src/test/java/io/confluent/kafka/connect/datagen/DatagenTaskTest.java
@@ -136,6 +136,23 @@ public class DatagenTaskTest {
   }
 
   @Test
+  public void shouldSetTaskGeneration()  throws Exception {
+    // This co-opts some int fields in one of the schemas
+    config.put(DatagenConnectorConfig.SCHEMA_TASK_GENERATION_FIELD_CONF, "stars");
+    config.put(DatagenConnectorConfig.SCHEMA_TASK_ID_FIELD_CONF, "route_id");
+    config.put(DatagenConnectorConfig.SCHEMA_PARTITION_FIELD_CONF, "route_id");
+    // Run the task and assert that the records have the values we injected.
+    createTaskWith(Quickstart.RATINGS);
+    generateRecords();
+    for (SourceRecord record : records) {
+      assertEquals(TASK_ID, record.kafkaPartition().intValue());
+      Struct struct = (Struct) record.value();
+      assertEquals(TASK_ID, struct.get("route_id"));
+      assertEquals(0, struct.get("stars"));
+    }
+  }
+
+  @Test
   public void shouldFailToGenerateMoreRecordsThanSpecified() throws Exception {
     // Generate the expected number of records
     createTaskWith(DatagenTask.Quickstart.USERS);
