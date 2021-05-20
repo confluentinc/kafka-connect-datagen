35ad72a29beda3925d810eb377d0ecff19c8fdaf
Yash Mayya <yash.mayya@gmail.com>
2021-05-20 10:39:41 +0530

    Log exceptions; Use try-with-resources

--- src/main/java/io/confluent/kafka/connect/datagen/ConfigUtils.java
+++ src/main/java/io/confluent/kafka/connect/datagen/ConfigUtils.java
@@ -24,14 +24,19 @@ import org.apache.avro.Schema;
 import org.apache.avro.Schema.Parser;
 import org.apache.avro.SchemaParseException;
 import org.apache.kafka.common.config.ConfigException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ConfigUtils {
+  private static final Logger log = LoggerFactory.getLogger(ConfigUtils.class);
+
   public static Schema getSchemaFromSchemaString(String schemaString) {
     Schema.Parser schemaParser = new Parser();
     Schema schema;
     try {
       schema = schemaParser.parse(schemaString);
     } catch (SchemaParseException e) {
+      log.error("Unable to parse the provided schema", e);
       throw new ConfigException("Unable to parse the provided schema");
     }
     return schema;
@@ -40,8 +45,7 @@ public class ConfigUtils {
   public static Schema getSchemaFromSchemaFileName(String schemaFileName) {
     Schema.Parser schemaParser = new Parser();
     Schema schema;
-    try {
-      InputStream stream = new FileInputStream(schemaFileName);
+    try (InputStream stream = new FileInputStream(schemaFileName)) {
       schema = schemaParser.parse(stream);
     } catch (FileNotFoundException e) {
       try {
@@ -53,9 +57,11 @@ public class ConfigUtils {
           DatagenTask.class.getClassLoader().getResourceAsStream(schemaFileName)
         );
       } catch (SchemaParseException | IOException i) {
+        log.error("Unable to parse the provided schema", i);
         throw new ConfigException("Unable to parse the provided schema");
       }
     } catch (SchemaParseException | IOException e) {
+      log.error("Unable to parse the provided schema", e);
       throw new ConfigException("Unable to parse the provided schema");
     }
     return schema;
